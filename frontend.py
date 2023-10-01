9a419015be48ce269fe609d32bfb62444241c1f6
martanascimento1 <mmns5@aluno.ifal.edu.br>
2023-10-01 12:39:27 -0300

    ;

--- frontend.py
+++ frontend.py
@@ -1,57 +1,92 @@
-from tkinter import *
-from tkinter.ttk import *
-from tkinter import messagebox
- 
-def inserir():
-  p = txt_p.get()
-  q = txt_q.get()
-  
-  #if(usuario == "admin" and senha == "1234"):
-  #  messagebox.showinfo("Aviso", "Usuário e senha corretos")
-  #else:
-   # messagebox.showinfo("Aviso", "Usuário e senha não conferem")
- 
-def main():
-  global inicial
-  global txt_p
-  global txt_q
- 
-  # vamos criar a tela de login
-  inicial = Tk()
- 
-  # vamos definir o tamanho da janela
-  inicial.geometry("260x120")
- 
-  # o titulo da janela
-  inicial.title("Criptografia RSA")
- 
-  # evitamos que a janela seja redimensionada
-  inicial.resizable(False, False)
- 
-  # vamos configurar o grid
-  inicial.columnconfigure(0, weight=1)
-  inicial.columnconfigure(1, weight=3)
- 
-  # nome do usuário
-  label_usuario = Label(inicial, text="Usuário:")
-  label_usuario.grid(column=0, row=0, sticky=W, padx=15, pady=10)
-  txt_p = Entry(inicial, width=28)
-  txt_p.grid(column=1, row=0, sticky=E, padx=15, pady=10)
- 
-  # senha
-  label_senha = Label(inicial, text="Senha:")
-  label_senha.grid(column=0, row=1, sticky=W, padx=15, pady=0)
-  txt_q = Entry(inicial, show="*", width=28)
-  txt_q.grid(column=1, row=1, sticky=E, padx=15, pady=0)
- 
-  # botão de login
-  btn_login = Button(inicial, text="Entrar", command=autenticar_usuario)
-  btn_login.grid(column=1, row=3, sticky=E, padx=15, pady=10)
- 
-  # entramos no loop de eventos 
-  inicial.mainloop() 
- 
-if __name__== "__main__":
-  main()           
-        
-        
+import tkinter as tk
+from tkinter import ttk
+import subprocess
+import json
+
+# Função para executar o código C e lidar com a resposta
+def executar_codigo_c(entrada):
+    try:
+        processo = subprocess.Popen(['./seu_programa_em_c'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = processo.communicate(input=json.dumps(entrada))
+        return stdout
+    except Exception as e:
+        return str(e)
+
+# Função para lidar com a escolha do usuário
+def selecionar_opcao():
+    opcao = opcao_var.get()
+    
+    if opcao == 1:
+        # Gerar chave pública
+        resultado = executar_codigo_c({'opcao': 'gerar_chave_publica'})
+        resultado_text.config(state=tk.NORMAL)
+        resultado_text.delete('1.0', tk.END)
+        resultado_text.insert(tk.END, resultado)
+        resultado_text.config(state=tk.DISABLED)
+    elif opcao == 2:
+        # Encriptar
+        mensagem = mensagem_entry.get()
+        chave_publica = chave_publica_entry.get()
+        entrada = {'opcao': 'encriptar', 'mensagem': mensagem, 'chave_publica': chave_publica}
+        resultado = executar_codigo_c(entrada)
+        resultado_text.config(state=tk.NORMAL)
+        resultado_text.delete('1.0', tk.END)
+        resultado_text.insert(tk.END, resultado)
+        resultado_text.config(state=tk.DISABLED)
+    elif opcao == 3:
+        # Desencriptar
+        chave_privada = chave_privada_entry.get()
+        entrada = {'opcao': 'desencriptar', 'chave_privada': chave_privada}
+        resultado = executar_codigo_c(entrada)
+        resultado_text.config(state=tk.NORMAL)
+        resultado_text.delete('1.0', tk.END)
+        resultado_text.insert(tk.END, resultado)
+        resultado_text.config(state=tk.DISABLED)
+
+# Configurar a janela principal
+root = tk.Tk()
+root.title("Criptografia RSA")
+
+# Configurar a escolha do usuário
+opcao_label = ttk.Label(root, text="Escolha uma opção:")
+opcao_label.pack()
+
+opcao_var = tk.IntVar()
+opcao_var.set(1)
+
+opcao_gerar_chave = ttk.Radiobutton(root, text="Gerar chave pública", variable=opcao_var, value=1)
+opcao_encriptar = ttk.Radiobutton(root, text="Encriptar", variable=opcao_var, value=2)
+opcao_desencriptar = ttk.Radiobutton(root, text="Desencriptar", variable=opcao_var, value=3)
+
+opcao_gerar_chave.pack()
+opcao_encriptar.pack()
+opcao_desencriptar.pack()
+
+# Configurar entrada de dados
+mensagem_label = ttk.Label(root, text="Mensagem:")
+mensagem_label.pack()
+
+mensagem_entry = ttk.Entry(root)
+mensagem_entry.pack()
+
+chave_publica_label = ttk.Label(root, text="Chave Pública:")
+chave_publica_label.pack()
+
+chave_publica_entry = ttk.Entry(root)
+chave_publica_entry.pack()
+
+chave_privada_label = ttk.Label(root, text="Chave Privada:")
+chave_privada_label.pack()
+
+chave_privada_entry = ttk.Entry(root)
+chave_privada_entry.pack()
+
+# Botão para executar a ação selecionada
+executar_button = ttk.Button(root, text="Executar", command=selecionar_opcao)
+executar_button.pack()
+
+# Área para exibir o resultado
+resultado_text = tk.Text(root, height=10, width=50, state=tk.DISABLED)
+resultado_text.pack()
+
+root.mainloop()
--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 janela = Tk ()
@@ -9,13 +9,13 @@ janela.geometry('700x400')
 #label = Label (janela, text = "Primeiro entry")
 #label.grid (column = 0, row = 0, padx=15, pady=15)
 
-#entrada = Entry(janela,width=10)
-#entrada.grid(column=1, row=0, padx=5, pady=15)
+entrada = Entry(janela,width=10)
+entrada.grid(column=1, row=0, padx=5, pady=15)
 
 def ola():
     resultado = entrada.get()
-    label.configure(text= resultado)
-texto_one = Label(janela, text="Escolha uma das opcoes a seguir:", bg='green', font='times')
+    #label.configure(text= resultado)
+texto_one = Label(janela, text="Escolha uma das opcoes a seguir:")
 texto_one.grid(column=10, row=0)
 botao = Button(janela, text="Gerar Chave Publica",command = ola)
 botao.grid(column=10, row=1, padx=10, pady=15)
